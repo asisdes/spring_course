2c8fad64ff33d1ac9f47f090991e664b629c64ba
Aziz <asisdes@mail.ru>
2022-05-05 01:22:12 +0600

    Add class Person DI

--- src/spring_introduction/tes1.java
+++ src/spring_introduction/tes1.java
@@ -1,10 +1,12 @@
 package spring_introduction;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class tes1 {
 
     public static void main(String[] args) {
 
-        Pet pet = new Cat();
+        Pet pet = new Dog();
         pet.say();
 
     }
