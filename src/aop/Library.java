e42fe2ff707493c6a9ae425eafae8796a96998a7
Aziz <asisdes@mail.ru>
2022-06-15 00:55:01 +0600

    Add AOP

--- src/aop/Library.java
+++ src/aop/Library.java
@@ -1,6 +1,15 @@
 package aop;
 
+import jdk.swing.interop.SwingInterOpUtils;
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
 public class Library {
 
+    public void getBook() {
+
+        System.out.println("Book is vzata");
+
+    }
 
 }
